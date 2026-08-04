656dc2cbf6b717f7038dc0fe1515d5a6376761a0
Oliver Maglana <jamols09@gmail.com>
2026-08-04 17:24:33 +0800

    feat: move checkout page to React

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,14 @@
 import { HomePage } from './pages/HomePage'
 import './App.css'
 import { Routes,Route } from 'react-router'
+import { CheckoutPage } from './pages/CheckoutPage'
 
 function App() {
 
   return (
     <Routes>
       <Route path="/" element={<HomePage />} />
-      <Route path="checkout" element={<div>test</div>} />
+      <Route path="checkout" element={<CheckoutPage/>} />
     </Routes>
   )
 }
