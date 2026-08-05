26bcb6e4d1aeb5fa33d7f9d07cb4d589d13592e1
Oliver Maglana <jamols09@gmail.com>
2026-08-05 15:36:25 +0800

    feat: move price conversion to util

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,6 +1,7 @@
 
 import { Header } from '../components/Header'
 import { useEffect, useState } from 'react';
+import { formatMoney } from '../utils/money';
 import axios from 'axios'
 import './HomePage.css';
 
@@ -47,7 +48,7 @@ export function HomePage({ cart }) {
                                 </div>
 
                                 <div className="product-price">
-                                    ${(product.priceCents / 100).toFixed(2)}
+                                    {formatMoney(product.priceCents)}
                                 </div>
 
                                 <div className="product-quantity-container">
